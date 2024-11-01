3ecdbbd472819bd2d375e8437f010f1557238400
Rita Onwudiwe <onwudiwechinwendu16@gmail.com>
2024-11-01 07:59:01 +0100

    Create Facebook_developer.py

---
+++ docs/links/Facebook_developer.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "Facebook Developer" 
+link_text = "Facebook Developer" 
+link_url = "https://developers.facebook.com/products/facebook-login/" 
+
+link.xref_links.update({link_name: (link_text, link_url)})
