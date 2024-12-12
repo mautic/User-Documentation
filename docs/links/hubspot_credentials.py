ea769a98630d7728bd0abf8cd49767c3e387db59
NormanPracht <npr@webmecanik.com>
2024-12-12 09:36:17 +0100

    add new hubspot sync info

---
+++ docs/links/hubspot_credentials.py
@@ -0,0 +1,7 @@
+from . import link
+
+link_name = "Hubspot Credentials"
+link_text = "get credentials by following the Hubspot documentation"
+link_url = "https://developers.hubspot.com/docs/api/private-apps"
+
+link.xref_links.update({link_name: (link_text, link_url)})
